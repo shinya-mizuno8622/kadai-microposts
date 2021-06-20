5d318e8d5ae7616db8d2283561aa3d07b3f9f777
shinya.mizuno <mizuno3109@yahoo.co.jp>
2021-06-20 07:48:51 +0000

    delete-useless

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,7 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :microposts, only: [:index, :show ,:create] do
-  end
-  
-  
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts, only: [:index, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
   resources :favorites,only: [:create, :destroy]
 end
